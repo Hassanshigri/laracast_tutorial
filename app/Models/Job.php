880126345c57c5fa43a1a1d2f1a18c72be9193fc
Muhammad Hassan <96192977+Hassanshigri@users.noreply.github.com>
2024-10-12 11:03:51 +0500

    day 9 Eloquent

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,39 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Arr;
 
-class Job{
-    public static function all(): array{
-        return[
-            [
-                'id'=>1,
-                'title'=>'director',
-                'salary'=>'$5000',
-            ],
-            [
-                'id'=>2,
-                'title'=>'Programmer',
-                'salary'=>'$1000',
-            ],
-            [
-                'id'=>3,
-                'title'=>'Teacher',
-                'salary'=>'$4000',
-            ],
-        ];
-    }
+class Job extends Model {
+    protected $table = 'job_lisktings';
 
-    public static function find($id):array
-    {
-        $job = Arr::first(static::all(), fn($job) => $job['id'] == $id);
-
-        if(! $job)
-        {
-            abort(404);
-        }
-        return $job;
-    }
+    protected $fillable = ['title', 'sallary'];
 }
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,8 @@ use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    // $jobs = Job::all();
+    // dd($jobs);
     return view('home');
 });
 
